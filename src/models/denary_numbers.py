a42e6f5286a4bb460724ba2f09c1c90a8056fa52
pchod <pawel23@ru.is>
2023-12-28 19:28:58 +0000

    created denary_numbers.py - working on FractionalNumbers class methods

---
+++ src/models/denary_numbers.py
@@ -0,0 +1,22 @@
+# denary_numbers.py
+
+
+class FractionalNumber:
+    """ """
+
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+        self.bin_denominator = bin(self.denominator)
+
+    def is_den_power_of_2(self):
+        # self.bin_denominator_reverse = self.bin_denominator - 1
+        # if (self.bin_denominator and self.bin_denominator_reverse) == False
+        #    return True
+        pass
+
+
+class DecimalNumber:
+    """ """
+
+    pass
