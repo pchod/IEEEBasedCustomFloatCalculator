1e3b8cf1ed6bce14372dc1ac88fef3bfc2409476
pchod <pawel23@ru.is>
2023-12-29 14:10:21 +0000

    working on FractionalNumber class

--- src/models/denary_numbers.py
+++ src/models/denary_numbers.py
@@ -8,11 +8,12 @@ class FractionalNumber:
         self.numerator = numerator
         self.denominator = denominator
         self.bin_denominator = bin(self.denominator)
+        self.bin_denominator_subtracted = bin(self.denominator - 1)
+
+    def to_binary(self):
+        pass
 
     def is_den_power_of_2(self):
-        # self.bin_denominator_reverse = self.bin_denominator - 1
-        # if (self.bin_denominator and self.bin_denominator_reverse) == False
-        #    return True
         pass
 
 
