4bfe093a3808c448dd56a2f323af9b28e3092325
pchod <pawel23@ru.is>
2024-01-04 19:46:24 +0000

    ieee_format.py updated

--- src/models/denary_numbers.py
+++ src/models/denary_numbers.py
@@ -7,17 +7,21 @@ class FractionalNumber:
     def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
-        self.bin_denominator = bin(self.denominator)
-        self.bin_denominator_subtracted = bin(self.denominator - 1)
 
-    def to_binary(self):
-        pass
+        # self.bin_denominator = bin(self.denominator)
+        # self.bin_denominator_subtracted = bin(self.denominator - 1)
 
-    def is_den_power_of_2(self):
-        pass
+    # def to_binary(self):
+
+    # def is_den_power_of_2(self):
 
 
 class DecimalNumber:
     """ """
 
-    pass
+    def __init__(self, decimal_number):
+        self.decimal_number = decimal_number
+        self.int_part = int(self.decimal_number)
+        # bool for sign
+
+        # needs to add abs() self.fractional_part = self.decimal_number - self.int_part
--- src/models/ieee_format.py
+++ src/models/ieee_format.py
@@ -6,7 +6,42 @@ class IEEEFormat:
 
     SIGN_BIT_LENGTH = 1
 
-    def __init__(self, exponent_length, mantissa_length):
+    def __init__(self, exponent_length: int, mantissa_length: int):
         self.exponent_length = exponent_length
         self.mantissa_length = mantissa_length
-        self.bias = 2 ** (len(str(exponent_length)) - 1) - 1
+        self.bias = 2 ** (exponent_length - 1) - 1
+
+
+class IEEE16BitFormat(IEEEFormat):
+    """Class representing 16 bit float representation IEEE 754 (half-precistion), binary16"""
+
+    def __init__(self):
+        super().__init__(exponent_length=5, mantissa_length=10)
+
+
+class IEEE32BitFormat(IEEEFormat):
+    """Class representing 32 bit float representation IEEE 754 (single precision), binary32"""
+
+    def __init__(self):
+        super().__init__(exponent_length=8, mantissa_length=23)
+
+
+class IEEE64BitFormat(IEEEFormat):
+    """Class representing 64 bit float representation IEEE 754 (double precision), binary64"""
+
+    def __init__(self):
+        super().__init__(exponent_length=13, mantissa_length=50)
+
+
+class IEEE128BitFormat(IEEEFormat):
+    """Class representing 128 bit float representation IEEE 754 (quadruple precision), binary128"""
+
+    def __init__(self):
+        super().__init__(exponent_length=17, mantissa_length=110)
+
+
+class IEEECustomLengthFormat(IEEEFormat):
+    """Class representing a custom bit-length float representation based on IEEE 754"""
+
+    def __init__(self, exponent_length, mantissa_length):
+        super().__init__(exponent_length, mantissa_length)
