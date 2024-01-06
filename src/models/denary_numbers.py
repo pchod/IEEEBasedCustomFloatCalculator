02ea9131b9465ee5c370665b86fb63a3692bdc6d
pchod <pawel23@ru.is>
2024-01-06 00:46:55 +0000

    denary_numbers.py updated - corrected logic for handling denary numbers

--- src/models/denary_numbers.py
+++ src/models/denary_numbers.py
@@ -1,13 +1,22 @@
 # denary_numbers.py
 
 
-class FractionalNumber:
+class DenaryNumber:
+    """A parent class for representation of denary numbers"""
+
+    def convert_to_fractional(self):
+        raise NotImplementedError
+
+    def convert_to_decimal(self):
+        raise NotImplementedError
+
+
+class FractionalNumber(DenaryNumber):
     """ """
 
-    def __init__(self, numerator, denominator):
+    def __init__(self, numerator: int, denominator: int):
         self.numerator = numerator
         self.denominator = denominator
-
         # self.bin_denominator = bin(self.denominator)
         # self.bin_denominator_subtracted = bin(self.denominator - 1)
 
@@ -16,12 +25,11 @@ class FractionalNumber:
     # def is_den_power_of_2(self):
 
 
-class DecimalNumber:
+class DecimalNumber(DenaryNumber):
     """ """
 
-    def __init__(self, decimal_number):
+    def __init__(self, decimal_number: float):
         self.decimal_number = decimal_number
+        self.is_positive = decimal_number > 0
         self.int_part = int(self.decimal_number)
-        # bool for sign
-
-        # needs to add abs() self.fractional_part = self.decimal_number - self.int_part
+        self.decimal_part = abs(self.decimal_number) - abs(self.int_part)
