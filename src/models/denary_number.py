7fc31aaf384d09157158e728a110c6766f215f34
pchod <pawel23@ru.is>
2024-01-07 01:29:49 +0000

    updates

--- src/models/denary_number.py
+++ src/models/denary_number.py
@@ -1,6 +1,9 @@
 # denary_number.py
 
 
+from models.ieee_format import IEEEFormat
+
+
 class DenaryNumber:
     """A parent class for representation of denary numbers"""
 
@@ -10,17 +13,49 @@ class DenaryNumber:
     def convert_to_decimal(self):
         raise NotImplementedError
 
+    def convert_to_binary_fraction(self):
+        raise NotImplementedError
+
 
 class FractionalNumber(DenaryNumber):
     """ """
 
     def __init__(self, numerator: int, denominator: int):
-        self.numerator = numerator
-        self.denominator = denominator
+        self.is_positive = not ((numerator < 0) ^ (denominator < 0))
+        self.numerator = abs(numerator)
+        assert denominator != 0, "The denominator cannot be 0"
+        self.denominator = abs(denominator)
         # self.bin_denominator = bin(self.denominator)
         # self.bin_denominator_subtracted = bin(self.denominator - 1)
 
-    # def to_binary(self):
+    def convert_parts_to_binary(self):
+        """Converts a fractional denary number into binary fraction."""
+
+        def convert_to_binary(num):
+            bin_temp = []
+            while num != 0:
+                remainder = num % 2
+                bin_temp.append(str(remainder))
+                num = num // 2
+
+            bin_temp.reverse()
+            bin_num = "".join(bin_temp) if bin_temp else "0"
+            return bin_num
+
+        numerator_bin = convert_to_binary(self.numerator)
+        denominator_bin = convert_to_binary(self.denominator)
+        return numerator_bin, denominator_bin
+
+    def convert_to_binary_fraction(self, ieee_format: IEEEFormat):
+        bin_numerator, bin_denoniminator = self.convert_parts_to_binary()
+        bin_numerator.lstrip("0")
+        bin_denoniminator.lstrip("0")
+        bin_fraction = ""
+        num_divisible_part = ""
+        for bit in bin_numerator:
+            num_divisible_part.join(bit)
+            if num_divisible_part <= bin_denoniminator:
+                break
 
     # def is_den_power_of_2(self):
 
--- src/models/ieee_format.py
+++ src/models/ieee_format.py
@@ -10,6 +10,10 @@ class IEEEFormat:
         self.exponent_length = exponent_length
         self.mantissa_length = mantissa_length
         self.bias = 2 ** (exponent_length - 1) - 1
+        self.total_bit_length = (
+            self.SIGN_BIT_LENGTH + self.exponent_length + self.mantissa_length
+        )
+        self.minimum_exp = 2 ** (1 - self.bias)
 
 
 class IEEE16BitFormat(IEEEFormat):
@@ -33,15 +37,18 @@ class IEEE64BitFormat(IEEEFormat):
         super().__init__(exponent_length=13, mantissa_length=50)
 
 
-class IEEE128BitFormat(IEEEFormat):
-    """Class representing 128 bit float representation IEEE 754 (quadruple precision), binary128"""
-
-    def __init__(self):
-        super().__init__(exponent_length=17, mantissa_length=110)
-
-
 class IEEECustomLengthFormat(IEEEFormat):
-    """Class representing a custom bit-length float representation based on IEEE 754"""
+    """Class representing a custom bit-length float representation based on IEEE 754.
+    It has to be at least 3"""
 
-    def __init__(self, exponent_length, mantissa_length):
+    def __init__(self, exponent_length: int, mantissa_length: int):
         super().__init__(exponent_length, mantissa_length)
+        assert (
+            (1 <= self.exponent_length <= 62)
+            and (1 <= self.mantissa_length <= 62)
+            and (3 <= self.total_bit_length <= 64)
+        ), (
+            "For the custom calculator mode: the exponent and mantissa"
+            " bit-length has to be in the range from 1 to 63 and their sum"
+            " cannot exceed 64 in total"
+        )
