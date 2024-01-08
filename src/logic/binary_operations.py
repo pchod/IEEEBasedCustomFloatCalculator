4f180379e931551f0623cf3f66fe305969599715
pchod <pawel23@ru.is>
2024-01-08 00:41:37 +0000

    logic updated, minor fixes

--- src/logic/binary_operations.py
+++ src/logic/binary_operations.py
@@ -19,18 +19,6 @@ class BinaryOperations:
         pass
 
     @staticmethod
-    def convert_fractional_part_to_binary(num):
-        bin_temp = []
-        while num != 0:
-            remainder = num % 2
-            bin_temp.append(str(remainder))
-            num = num // 2
-
-        bin_temp.reverse()
-        bin_num = "".join(bin_temp) if bin_temp else "0"
-        return bin_num
-
-    @staticmethod
     def compare_bin_lengths(binary_1: str, binary_2: str):
         if len(binary_1) > len(binary_2):
             return 1
@@ -55,6 +43,13 @@ class BinaryOperations:
             left_padding = "0" * (len(binary_2) - len(binary_1))
             binary_2 = left_padding + binary_2
             return binary_1, binary_2
+
+    @staticmethod
+    def right_zero_pad(fraction_part_bin: str, ieee_format: IEEEFormat):
+        """Padding with zeros on the right to prepare for the fraction division with given precision"""
+        while len(fraction_part_bin) < ieee_format.mantissa_length:
+            fraction_part_bin += "0"
+        return fraction_part_bin
 
     @staticmethod
     def subtract_binaries(binary_1: str, binary_2: str):
@@ -90,43 +85,101 @@ class BinaryOperations:
         return subtraction_product
 
     @staticmethod
-    def convert_to_binary_fraction(
-        denominator_bin: str, numerator_bin: str, ieee_format: IEEEFormat
+    def convert_to_binary_fraction_whole_part(
+        denominator_bin: str, numerator_bin: str
     ):
+        """Performing a division to get the whole part of the binary fraction. Returns the whole part
+        and the remainder for the further division for fractional part"""
         numerator_bin = numerator_bin.lstrip("0")
         denominator_bin = denominator_bin.lstrip("0")
-        fraction_bin = ""
         whole_part_bin = ""
-        fractional_part_bin = ""
-        dividend_zero_padding = ""
-        # if len(numerator_bin) <= len(denominator_bin):
-        #    dividend_zero_padding = "0" * (len(denominator_bin) - len(numerator_bin))
-        # numerator_bin = dividend_zero_padding + numerator_bin
-
-        # whole part division
-        remainder = ""
-        divisible_num_part = ""
+        remainder_bin = ""
         for bit in numerator_bin:
-            divisible_num_part += bit
-            if divisible_num_part >= denominator_bin:
+            remainder_bin += bit
+            if remainder_bin >= denominator_bin:
                 whole_part_bin += "1"
-                divisible_num_part = subtract_binary(
-                    divisible_num_part, denominator_bin
+                remainder_bin, denominator_bin = (
+                    BinaryOperations.left_zero_pad_shorter_bin(
+                        remainder_bin, denominator_bin
+                    )
                 )
+                remainder_bin = BinaryOperations.subtract_binaries(
+                    remainder_bin, denominator_bin
+                )
+            else:
+                whole_part_bin += "0"
+        whole_part_bin = whole_part_bin.lstrip("0")
+        return whole_part_bin, remainder_bin
 
     @staticmethod
-    def convert_fractional_to_binary_fraction(
-        numerator: int, denominator: int, ieee_format: IEEEFormat
+    def convert_to_binary_fraction_fraction_part(
+        numerator_bin: str, remainder_bin: str, ieee_format: IEEEFormat
     ):
-        pass
+        if not remainder_bin:
+            return "0"
+        numerator_bin = numerator_bin.lstrip("0")
+        remainder_bin += BinaryOperations.right_zero_pad(
+            remainder_bin, ieee_format
+        )
+        fractional_part_bin = ""
+        temp_remainder = ""
+        is_rounded = False
+        # division until reaches
+        # needs optimization in the next iteration - when the division is complete to pad with zeros
+        # the loop goes until
+        while (
+            len(fractional_part_bin.split("1", 1)[0])
+            < ieee_format.max_normalised_exp_int + 1
+        ) or (
+            len(fractional_part_bin) < ieee_format.max_normalised_exp_int + 1
+            and "1" not in fractional_part_bin
+        ):
+            for bit in numerator_bin:
+                fractional_part_bin += bit
+                if temp_remainder >= remainder_bin:
+                    fractional_part_bin += "1"
+                    temp_remainder, remainder_bin = (
+                        BinaryOperations.left_zero_pad_shorter_bin(
+                            temp_remainder, remainder_bin
+                        )
+                    )
+                    temp_remainder = BinaryOperations.subtract_binaries(
+                        temp_remainder, remainder_bin
+                    )
+                else:
+                    fractional_part_bin += "0"
+        fractional_part_bin = "".join(fractional_part_bin)
+        # rounding part
+        if fractional_part_bin[-1] == "1":
+            if fractional_part_bin[-2] == "0":
+                fractional_part_bin = fractional_part_bin[:-2]
+                fractional_part_bin += "1"
+                is_rounded = True
+            else:
+                fractional_part_bin = fractional_part_bin[:-1]
+        else:
+            fractional_part_bin = fractional_part_bin[:-1]
+
+        return fractional_part_bin, is_rounded
 
     @staticmethod
-    def convert_decimal_to_binary_fraction():
-        pass
+    def normalise_binary_fraction(
+        fractional_part_bin: str, ieee_format: IEEEFormat
+    ):
+        """"""
+        was_normalised = False
+        if fractional_part_bin[0] == "1":
+            return fractional_part_bin, was_normalised
 
-    @staticmethod
-    def normalise_binary_fraction():
-        pass
+        else:
+            split_parts = fractional_part_bin.split("1", 1)[0]
+            left_shift = len(split_parts[0])
+            fractional_part_bin = split_parts[1]
+            fractional_part_bin = BinaryOperations.right_zero_pad(
+                fractional_part_bin, ieee_format
+            )
+            was_normalised = True
+            return fractional_part_bin, left_shift, was_normalised
 
     @staticmethod
     def convert_from_binary_fraction_to_IEEE_float():
