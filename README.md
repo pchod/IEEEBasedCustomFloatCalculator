ecd18f1c0939c1afcffbd0caee916833c46f43f0
pchod <pawel23@ru.is>
2024-01-13 12:42:38 +0100

    normalisation logic added

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ Conversion denary numbers into IEEE 754 representation for arbitrary exponent an
 there is a lot of IEEE 754 calculators available on the web it was impossible to find the calculator to reliably check
 conversion to custom-length binary strings following IEEE 754 guidelines and rules of calculation.
 
+Moreover this project serves an educational purpose - all of the operations are performed on strings with utilizing no external libraries.
+This gave the crucial hand-on experience in working on strings mimicking the bit operations in low-level languages.
+
+The project is being designed mostly as an educational tool for high school and university students. The program will display as well each step performed by the program
+along with relevant values (remainders, quotients, information about rounding, loss of precision, etc.)
+
 The UI part will be designed in flask, while the whole backend I wrote in vanilla python.
 
 ## Status
@@ -24,4 +30,6 @@ unit tests and work as designed.
 
 ## Technical Overview
 
+The program is designed in the MVC (Model - View - Controller) approach.
 
+## Testing
--- src/logic/binary_operations.py
+++ src/logic/binary_operations.py
@@ -58,7 +58,7 @@ class BinaryOperations:
     @staticmethod
     def subtract_binaries(binary_1: str, binary_2: str):
         """Subtracting binary_1 - binary_2 using bitwise operations. It is guaranteed, that binary_1 > binary_2 - need to add the check"""
-        #ic(binary_1, binary_2)
+        # ic(binary_1, binary_2)
         subtraction_product = []
         borrow = False
 
@@ -67,38 +67,38 @@ class BinaryOperations:
             # ic(subtraction_product)
             bit_binary_1 = binary_1[index]
             bit_binary_2 = binary_2[index]
-            #print()
-            #print("state before subtraction, checking remaining borrow based on the previous iteration")
-            #ic(bit_binary_1, bit_binary_2, borrow)
-            #print()
-            #print("state after checking borrow")
-            #ic(bit_binary_1, bit_binary_2, borrow)
-            #print()
-            #print("starting subtraction")
-            #ic(bit_binary_1, bit_binary_2, borrow)
+            # print()
+            # print("state before subtraction, checking remaining borrow based on the previous iteration")
+            # ic(bit_binary_1, bit_binary_2, borrow)
+            # print()
+            # print("state after checking borrow")
+            # ic(bit_binary_1, bit_binary_2, borrow)
+            # print()
+            # print("starting subtraction")
+            # ic(bit_binary_1, bit_binary_2, borrow)
             # checked and correct
             if bit_binary_1 < bit_binary_2:
-                #print("bit 1 is lower than bit 2")
+                # print("bit 1 is lower than bit 2")
                 borrow = True
-                #ic(borrow)
+                # ic(borrow)
 
                 subtraction_product.append("1")
 
             elif bit_binary_1 > bit_binary_2:
                 subtraction_product.append("0" if borrow else "1")
-                #print("bit 1 is higher than bit 2")
+                # print("bit 1 is higher than bit 2")
                 if borrow:
                     borrow = False
 
             else:
                 # for equal bits the product is 0, if the bit is borrowed, be become 1
-                #print("bit 1 is equal to bit 2")
+                # print("bit 1 is equal to bit 2")
                 subtraction_product.append("1" if borrow else "0")
                 borrow = (bit_binary_1 == "0") and borrow
-        #ic(subtraction_product)
+        # ic(subtraction_product)
         subtraction_product_reversed = reversed(subtraction_product)
         subtraction_product_bin = "".join(subtraction_product_reversed)
-        #ic(subtraction_product_bin)
+        # ic(subtraction_product_bin)
         return subtraction_product_bin
 
     """@staticmethod
@@ -134,6 +134,7 @@ class BinaryOperations:
                 whole_part_bin += "0"
 
         return """
+
     @staticmethod
     def convert_to_binary_fraction_whole_part(
         numerator_bin: str, denominator_bin: str
@@ -145,7 +146,7 @@ class BinaryOperations:
         print()
         ic(numerator_bin, denominator_bin)
         numerator_bin = numerator_bin.lstrip("0")
-        if not numerator_bin: 
+        if not numerator_bin:
             numerator_bin = "0"
         # need for thoroygh testing
         if not denominator_bin:
@@ -158,17 +159,18 @@ class BinaryOperations:
         for i, bit in enumerate(numerator_bin):
             remainder_bin += bit
             ic(bit, remainder_bin, denominator_bin, whole_part_bin)
-            padded_remainder, padded_denominator = BinaryOperations.left_zero_pad_shorter_bin(
-                remainder_bin, denominator_bin
+            padded_remainder, padded_denominator = (
+                BinaryOperations.left_zero_pad_shorter_bin(
+                    remainder_bin, denominator_bin
+                )
             )
-            
+
             if padded_remainder >= padded_denominator:
-            
-                #remainder_bin, denominator_bin = (
+                # remainder_bin, denominator_bin = (
                 #    BinaryOperations.left_zero_pad_shorter_bin(
                 #        remainder_bin, denominator_part
                 #    )
-                #)
+                # )
                 ic(remainder_bin, padded_denominator)
                 remainder_bin = BinaryOperations.subtract_binaries(
                     padded_remainder, padded_denominator
@@ -186,21 +188,25 @@ class BinaryOperations:
             whole_part_bin = "0"
         ic(whole_part_bin, remainder_bin)
         return whole_part_bin, remainder_bin
-    
+
     @staticmethod
     def is_whole_part_zero(whole_part_bin: str):
         "Used to check if the conversion to IEEE format will utilize left shifts. Returns True if the whole part is zero and no left shift is needed"
         return True if not "1" in whole_part_bin else False
-    
+
     @staticmethod
     def convert_to_binary_fraction_fraction_part(
         remainder_after_whole_part: str,
         denominator_bin: str,
         is_whole_part_zero: bool,
         ieee_format: IEEEFormat,
-        
     ):
-        ic(remainder_after_whole_part, denominator_bin, is_whole_part_zero, ieee_format.mantissa_length)
+        ic(
+            remainder_after_whole_part,
+            denominator_bin,
+            is_whole_part_zero,
+            ieee_format.mantissa_length,
+        )
         if not remainder_after_whole_part or remainder_after_whole_part == "0":
             return "0"
         remainder_bin = ""
@@ -211,24 +217,40 @@ class BinaryOperations:
         i = 0
         digits_after_first_1 = 0
         first_1_found = not is_whole_part_zero
-        while (is_whole_part_zero and len(fractional_part_bin) < (ieee_format.mantissa_length + 1)) or (not first_1_found and len(fractional_part_bin) < (ieee_format.mantissa_length + 1)) or (first_1_found and digits_after_first_1 < (ieee_format.mantissa_length + 1)):
+        while (
+            (
+                is_whole_part_zero
+                and len(fractional_part_bin)
+                < (ieee_format.mantissa_length + 1)
+            )
+            or (
+                not first_1_found
+                and len(fractional_part_bin)
+                < (ieee_format.mantissa_length + 1)
+            )
+            or (
+                first_1_found
+                and digits_after_first_1 < (ieee_format.mantissa_length + 1)
+            )
+        ):
             if i < len(remainder_after_whole_part):
                 bit = remainder_after_whole_part[i]
             else:
                 bit = "0"
             remainder_bin += bit
             ic(bit, remainder_bin, denominator_bin)
-            padded_current_remainder, padded_denominator = BinaryOperations.left_zero_pad_shorter_bin(
-                remainder_bin, denominator_bin
+            padded_current_remainder, padded_denominator = (
+                BinaryOperations.left_zero_pad_shorter_bin(
+                    remainder_bin, denominator_bin
+                )
             )
-            
+
             if padded_current_remainder >= padded_denominator:
-            
-                #remainder_bin, denominator_bin = (
+                # remainder_bin, denominator_bin = (
                 #    BinaryOperations.left_zero_pad_shorter_bin(
                 #        remainder_bin, denominator_part
                 #    )
-                #)
+                # )
                 ic(remainder_bin, padded_denominator)
                 remainder_bin = BinaryOperations.subtract_binaries(
                     padded_current_remainder, padded_denominator
@@ -247,10 +269,19 @@ class BinaryOperations:
                 if first_1_found:
                     digits_after_first_1 += 1
             i += 1
-        ic(fractional_part_bin, padded_current_remainder, padded_denominator, first_1_found)
-        print(f"the IEEE length of mantissa is {ieee_format.mantissa_length}, while the actual length is {len(fractional_part_bin)}")
+        ic(
+            fractional_part_bin,
+            padded_current_remainder,
+            padded_denominator,
+            first_1_found,
+        )
+        print(
+            f"the IEEE length of mantissa is {ieee_format.mantissa_length},"
+            f" while the actual length is {len(fractional_part_bin)}"
+        )
         return fractional_part_bin
-    #def round_binary_fraction_part(fractional_part_bin: str, ieee_format: IEEEFormat):
+
+    # def round_binary_fraction_part(fractional_part_bin: str, ieee_format: IEEEFormat):
     #    """"""
     #    is_rounded = False
     #    if not fractional_part_bin or fractional_part_bin == "0":
@@ -264,35 +295,47 @@ class BinaryOperations:
     #        else:
     #           fractional_part_bin = fractional_part_bin[:-1]
     #            is_rounded = False
-        
 
     # seems reduntant with the is_whole_part_zero function - needs to check
     @staticmethod
-    def check_if_binary_needs_normalisation(
+    def check_need_to_normalise(
         whole_part_bin: str, fractional_part_bin: str, ieee_format: IEEEFormat
     ):
-        """ """
+        """Needs to add the parameter and conditions for subnormal numbers and special cases."""
+
         pass
-        
+
     @staticmethod
     def normalise_binary_fraction(
-        whole_part_bin: str, fractional_part_bin: str, ieee_format: IEEEFormat, is_whole_part_zero: bool):
-        
+        whole_part_bin: str,
+        fractional_part_bin: str,
+        ieee_format: IEEEFormat,
+        to_normalise: bool,
+    ):
         """Used for normal numbers to convert to IEEE. need to add function for checking the normalisation range"""
-        was_normalised = False
-        exp_bin = ""
-        # needs all variables to print out
-        ic(whole_part_bin, fractional_part_bin, ieee_format.mantissa)
-        if not fractional_part_bin or fractional_part_bin == "0":
-            fractional_part_bin = BinaryOperations.right_zero_pad(fraction_part_bin=fractional_part_bin, ieee_format=ieee_format)
+        actual_exponent = 0
+        calculated_exponent = 0
+        normalised_fraction = ""
+        # normalisation_range to add - range of numbers that can be represented as exponent in the IEEE format
+
+        # removing leading irrelevant zeros from the whole part binary
         whole_part_bin = whole_part_bin.lstrip("0")
-        if not whole_part_bin:
-            whole_part_bin = "0"
-        
-        
-        if is_whole_part_zero:
-        ic(fraction_before_norm)
-        pass
+
+        # condition check to perform the right shift (if the whole part is not)
+        # XOR the left shift - if the whole part is 0.
+        if whole_part_bin:
+            normalised_fraction = whole_part_bin + fractional_part_bin
+            actual_exponent = len(whole_part_bin)
+            # necessary to add rounding to IEEE format after the right shift
+        else:
+            normalised_fraction = fractional_part_bin.lstrip("0")
+            actual_exponent = len(fractional_part_bin) - len(
+                normalised_fraction
+            )
+
+        # add the
+
+        return normalised_fraction, actual_exponent, calculated_exponent
 
     @staticmethod
     def convert_from_binary_fraction_to_IEEE_float():
