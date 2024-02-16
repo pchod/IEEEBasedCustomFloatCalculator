9f2b8836c8e980dcb9b7c09f6c64593880c526a3
pchod <pawel23@ru.is>
2024-02-16 18:27:04 +0000

    DenaryNumbers class updated and seems finished

--- src/models/denary_number.py
+++ src/models/denary_number.py
@@ -7,25 +7,6 @@ from models.ieee_format import IEEEFormat
 class DenaryNumber:
     """A parent class for representation of denary numbers"""
     
-    def __init__(self):
-        self.is_positive = None
-        self.numerator = None
-        self.denominator = None
-        self.simplified_numerator, self.simplified_denominator = self._simplify_fraction()
-        self.int_part = None
-        self.decimal_part = None
-        self.decimal_from_fractional = None
-        self.den_is_power_of_2 = self._is_den_power_of_2()
-
-    def convert_to_fractional(self):
-        raise NotImplementedError
-
-    def convert_to_decimal(self):
-        raise NotImplementedError
-
-    def convert_to_binary_fraction(self):
-        raise NotImplementedError
-    
     def _gcd(self, num=None, den=None):
         if num == None:
             numerator = self.numerator
@@ -59,29 +40,34 @@ class DenaryNumber:
         simplified_denominator //= gcd
         return simplified_numerator, simplified_denominator
     
-    def _is_den_power_of_2(self):
+    def _is_den_power_of_2(self, denominator=None):
         """Using bitwise operation to determine if the denominator is a power of 2. Needed for sticky bit calculation in rounding process
         while converting to binary fraction and IEEE 754 format"""
-        if self.simplified_denominator == 0:
-            raise ZeroDivisionError("The denominator cannot be 0")
-        
-        return True if self.simplified_denominator & (self.simplified_denominator - 1) == 0 else False
-
+        if denominator == None:
+            if self.simplified_denominator == 0:
+                raise ZeroDivisionError("The denominator cannot be 0")
+            return True if self.simplified_denominator & (self.simplified_denominator - 1) == 0 else False
+        if denominator:
+            if denominator == 0:
+                raise ZeroDivisionError("The denominator cannot be 0")
+            return True if denominator & (denominator - 1) == 0 else False
 
 class FractionalNumber(DenaryNumber):
     """ """
 
-    def __init__(self, numerator: int, denominator=1, is_positive=True):
-        if is_positive is None:
-            self.is_positive = not ((numerator < 0) ^ (denominator < 0))
+    def __init__(self, numerator: int, denominator=1):
+        self.is_positive = not ((numerator < 0) ^ (denominator < 0))
         self.numerator = abs(numerator)
         assert denominator != 0, "The denominator cannot be 0"
         self.denominator = abs(denominator)
         self.simplified_numerator, self.simplified_denominator = self._simplify_fraction()
         self.den_is_power_of_2 = self._is_den_power_of_2()
+        self.decimal_float = self._convert_to_decimal()
 
-    def __str__(self):
-        pass
+    def _convert_to_decimal(self):
+        """Returns int"""
+        decimal_float = self.numerator / self.denominator
+        return decimal_float
 
 
 class DecimalNumber(DenaryNumber):
@@ -93,9 +79,10 @@ class DecimalNumber(DenaryNumber):
         self.simplified_fractional_part = int(str(self.fractional_part).lstrip("0"))
         self.is_positive = is_positive if int_part >= 0 else False
         self.is_decimal_zero = self.int_part == 0 and self.simplified_fractional_part == 0
-        self.numerator, self.denominator = self.convert_to_fractional()
+        self.numerator, self.denominator = self._convert_to_fractional()
+        self.den_is_power_of_2 = self._is_den_power_of_2()
 
-    def convert_to_fractional(self):
+    def _convert_to_fractional(self):
         """Returns a tuple numerator, denominator"""
         if self.is_decimal_zero:
             return 0, 1
@@ -104,8 +91,4 @@ class DecimalNumber(DenaryNumber):
         else:
             denominator = int(10 * len(str(self.simplified_fractional_part)))
             numerator = self.int_part * denominator + self.simplified_fractional_part
-            return numerator, denominator
-
-        
-        
-        
+            return self._simplify_fraction(numerator, denominator)
