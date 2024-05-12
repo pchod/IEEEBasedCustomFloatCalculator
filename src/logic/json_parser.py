bf0569bab95f45338d869ac2c2b8f17102a511a7
pchod <pawel23@ru.is>
2024-05-12 20:27:15 +0000

    JSONParser formatting

--- src/logic/json_parser.py
+++ src/logic/json_parser.py
@@ -1,5 +1,6 @@
 import json
 
+
 class JSONParser:
     def __init__(self, json_file):
         with open(json_file, 'r') as file:
@@ -9,7 +10,7 @@ class JSONParser:
         path = self.json_data.get("path")
         return path
 
-    def parse_IEEE_format(self):
+    def parse_ieee_format(self):
         ieee_format = self.json_data.get("ieee_format")
         if ieee_format == "custom":
             exponent_length = self.json_data.get("exponent_length")
@@ -17,6 +18,7 @@ class JSONParser:
             return ieee_format, exponent_length, mantissa_length
         else:
             return ieee_format
+
     def parse_fractional(self):
         is_positive = self.json_data.get("is_positive")
         numerator = self.json_data.get("numerator")
@@ -29,9 +31,8 @@ class JSONParser:
         fract_part = self.json_data.get("fract_part")
         return is_positive, int_part, fract_part
 
-    def parse_IEEE_representation(self):
+    def parse_ieee_representation(self):
         sign_bit = self.json_data.get("sign_bit")
         exponent_bits = self.json_data.get("exponent_bits")
         mantissa_bits = self.json_data.get("mantissa_bits")
         return sign_bit, exponent_bits, mantissa_bits
-
