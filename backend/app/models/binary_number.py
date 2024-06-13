3b9d7ac54109d878394d6a5c85dec4d4868b2fe6
pchod <pawel23@ru.is>
2024-06-13 16:00:10 +0000

    binary number schema (marshmallow) added to schemas

--- backend/app/models/binary_number.py
+++ backend/app/models/binary_number.py
@@ -4,12 +4,13 @@ class BinaryNumber:
     """Represents a binary number in a scientific notation form"""
 
     def __init__(
-        self,
-        binary_whole_part: str = None,
-        binary_fraction: str = None,
-        is_positive: bool = None,
-        
+            self,
+            binary_whole_part: str = None,
+            binary_fraction: str = None,
+            is_positive: bool = None,
+
     ):
         self.binary_whole_part = binary_whole_part
         self.binary_fraction = binary_fraction
         self.is_positive = is_positive
+        self.sign_bit = "0" if is_positive else "1"
--- backend/app/schemas/binary_number_schema.py
+++ backend/app/schemas/binary_number_schema.py
@@ -1,2 +1,17 @@
 # binary_number_schema.py
 
+from marshmallow import Schema, fields
+
+
+class BinaryNumberSchema(Schema):
+    binary_whole_part = fields.Str(required=True)
+    binary_fraction = fields.Str(required=True)
+    is_positive = fields.Bool(required=True)
+    sign_bit = fields.Str(required=True)
+
+
+class BinaryNumberResponseSchema(Schema):
+    binary_whole_part = fields.Str(required=True)
+    binary_fraction = fields.Str(required=False, allow_none=True)
+    is_positive = fields.Bool(required=True)
+    sign_bit = fields.Str(required=True)
